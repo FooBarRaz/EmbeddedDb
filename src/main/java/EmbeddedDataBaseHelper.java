96719b7f3ec366c094d0a3331b98c6f6962718c9
Mehraz Ali <Mehraz Ali>
2016-07-03 14:42:24 -0400

    create database server instead of embedded file

--- src/main/java/EmbeddedDataBaseHelper.java
+++ src/main/java/EmbeddedDataBaseHelper.java
@@ -1,21 +1,25 @@
 import org.h2.jdbcx.JdbcConnectionPool;
+import org.h2.tools.Server;
 import org.skife.jdbi.v2.DBI;
 import org.skife.jdbi.v2.Handle;
 
-import javax.sql.ConnectionPoolDataSource;
 import javax.sql.DataSource;
-import java.io.PrintWriter;
-import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.SQLFeatureNotSupportedException;
-import java.util.Map;
-import java.util.logging.Logger;
 
-/**
- * Created by Raz on 7/3/2016.
- */
 public class EmbeddedDatabaseHelper {
 
+    private Server server;
+
+    public EmbeddedDatabaseHelper() {
+        server = null;
+        try {
+            server = Server.createTcpServer(
+                    "-tcpPort", "9192", "-tcpAllowOthers").start();
+        } catch (SQLException e) {
+
+        }
+
+    }
 
     public DataSource getDataSource() {
 
@@ -23,11 +27,15 @@ public class EmbeddedDatabaseHelper {
                 create("jdbc:h2:~/Documents/dev/EmbeddedDb/testdb0;AUTO_SERVER=TRUE", "sa", "");
 
 
-        Handle connectionHandle = DBI.open(cp);
-        connectionHandle.execute("create table if not exists foobar (id int primary key auto_increment, name varchar(100))");
-        connectionHandle.execute("insert into foobar (name) values (?)", "Jeff");
+//        Handle connectionHandle = DBI.open(cp);
+//        connectionHandle.execute("create table if not exists foobar (id int primary key auto_increment, name varchar(100))");
+//        connectionHandle.execute("insert into foobar (name) values (?)", "Jeff");
+//        connectionHandle.close();
 
         return cp;
     }
 
+    public void stopServer() {
+        server.stop();
+    }
 }
--- src/test/java/EmbeddedDatabaseHelperTest.java
+++ src/test/java/EmbeddedDatabaseHelperTest.java
@@ -1,20 +1,48 @@
+import org.h2.jdbcx.JdbcConnectionPool;
 import org.junit.Test;
 import org.skife.jdbi.v2.DBI;
 import org.skife.jdbi.v2.Handle;
 
+import java.util.List;
+import java.util.Map;
+
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsNull.notNullValue;
+import static org.hamcrest.core.Is.is;
 
 public class EmbeddedDatabaseHelperTest {
 
     @Test
-    public void constructor_initializesDataSource() throws Exception {
+    public void constructor_startsServer() throws Exception {
+        EmbeddedDatabaseHelper embeddedDatabaseHelper = new EmbeddedDatabaseHelper();
 
-        EmbeddedDatabaseHelper embdeddedDatabaseHelper = new EmbeddedDatabaseHelper();
+        JdbcConnectionPool cp = JdbcConnectionPool.
+                create("jdbc:h2:tcp://localhost:9192/~/Documents/dev/EmbeddedDb/testdb0", "sa", "");
 
-        Handle connectionHandler = DBI.open(embdeddedDatabaseHelper.getDataSource());
-        assertThat(connectionHandler, notNullValue());
+        Handle connectionHandler = DBI.open(cp);
+        connectionHandler.execute("create table if not exists foobar (id int primary key auto_increment, name varchar(100))");
+        connectionHandler.execute("insert into foobar (name) values (?)", "Bar");
+
+        List<Map<String, Object>> list = connectionHandler.createQuery("select * from foobar").list();
+
+
+
+        assertThat(connectionHandler.getConnection().isValid(100), is(true));
+        connectionHandler.close();
+        assertThat(connectionHandler.getConnection().isValid(100), is(false));
+
+        embeddedDatabaseHelper.stopServer();
     }
+
+//    @Test
+//    public void constructor_initializesDataSource() throws Exception {
+//        EmbeddedDatabaseHelper embeddedDatabaseHelper = new EmbeddedDatabaseHelper();
+//
+//        Handle connectionHandler = DBI.open(embeddedDatabaseHelper.getDataSource());
+//
+//        assertThat(connectionHandler.getConnection().isValid(100), is(true));
+//        connectionHandler.close();
+//        assertThat(connectionHandler.getConnection().isValid(100), is(false));
+//    }
 
 
 }
